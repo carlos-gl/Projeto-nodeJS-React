321813a3abc8785797d41eef175242dadebdd139
carlos-gl <carloslins170@gmail.com>
2020-03-29 22:06:27 -0300

    frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,9 +4,9 @@ const routes = require('./routes');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.use(cors());
 
-app.listen(8080);
+app.listen(3333);
 
